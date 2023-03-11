3408129380ec293501ff0863cc98c90bd6ef4eb8
eduardo45mp <eduardopeixoto45@outlook.com>
2023-03-11 14:45:09 -0300

    EmailJS ainda não configurado corretamente

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,15 +1,35 @@
-function sendEmail() {
-    let conteudo = document.getElementById("email").value + " " + document.getElementById("dprojeto").value;
-    Email.send({
-      Host: "smtp.gmail.com",
-      Username: "webboys.freela@gmail.com",
-      Password: "iJGE9Nzz9NkXEDU",
-      To: "eduardopeixoto45@outlook.com",
-      From: "webboys.freela@gmail.com",
-      Subject: document.getElementById("projeto").value,
-      Body: conteudo 
-    }).then(
-      message => alert("E-mail enviado com sucesso!")
-    );
+function sendEmail(event) {
+  emailjs.init("Uf-oO0lZ6F79nLi9w");
+  event.preventDefault();
+  
+  let nome = document.getElementById('nome').value;
+  let email = document.getElementById('email').value;
+  let assunto = document.getElementById('assunto').value;
+  let mensagem = document.getElementById('mensagem').value;
+  let contato = document.getElementById('contato').value;
+  
+  let emailData = {
+  to_name: 'WebBoys',
+  from_name: nome,
+  from_email: email,
+  subject: assunto,
+  message: mensagem,
+  };
+  
+  emailjs.sendForm('service_liq9ztn', 'contato', emailData)
+  .then(function() {
+      console.log('SUCCESS!');
+  }, function(error) {
+      console.log('FAILED...', error);
+  });
+  /*emailjs.send('service_liq9ztn', contato, emailData)
+  .then(function(response) {
+  console.log('SUCCESS!', response.status, response.text);
+  alert('Seu email foi enviado com sucesso!');
+  }, function(error) {
+  console.log('FAILED...', error);
+  alert('Ocorreu um erro ao enviar o seu email. Por favor, tente novamente mais tarde.');
+  });*/
+
   }
   
